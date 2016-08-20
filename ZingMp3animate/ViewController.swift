8ee631726bc51dd9918831baf9403ec305dd99b9
duthli <hung1996latoi2@gmail.com>
2016-08-20 14:24:30 +0700

    first commit

--- ZingMp3animate/ViewController.swift
+++ ZingMp3animate/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl_text: UILabel!
+    
+    @IBOutlet weak var logo: UIImageView!
+    var timer = NSTimer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.logo!.alpha = 0
+        self.lbl_text!.alpha = 0
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.2,target: self, selector: #selector(viewWillAppear),userInfo: nil,repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear( animated : Bool){
+        super.viewWillAppear(animated)
+        UIView.animateWithDuration(2, animations: {
+            self.logo!.alpha = 1
+        })
+        {
+            (finished) in UIView.animateWithDuration(1, animations:
+                {
+                    self.lbl_text!.center = CGPointMake(self.logo!.center.x, 8.0)
+                    self.lbl_text!.alpha = 1
 
+            })
+        
+        }
+    }
 }
 
